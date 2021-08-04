4c7b9172121304e3eb5a402176ecc2e4b581079b
iamrussl <livett.py@gmail.com>
2021-08-04 21:53:30 +0100

    initial upload of project files

---
+++ flaskblog.py
@@ -0,0 +1,56 @@
+# flask imports
+from flask import Flask # importing Flask
+from flask import render_template # importing the ability to render web pages
+from flask import url_for #
+from flask import flash # importing to allow for a message to display
+from flask import redirect # importing for the ability to redirect after a GET/POST request on submit, eg after login
+
+from forms import RegistrationForm, LoginForm
+
+# app config
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '3ce640eb16424716c5ebbed586dc2b29' # app.config is how you set values on the app
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# data import
+
+posts = [
+    {
+        'author' : 'Name 1',
+        'title' : 'Post 1',
+        'content' : 'First',
+        'date_posted' : 'August, 2021'
+    },
+    {
+        'author' : 'Name 2',
+        'title' : 'Post 2',
+        'content' : 'second',
+        'date_posted' : 'August, 2021'
+    }
+]
+
+# pages routing
+@app.route("/")
+@app.route('/home')
+def home_page():
+    return render_template('home.html', posts=posts, title='Home')
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html', title='About')
+
+# forms routing
+@app.route('/register', methods=['GET', 'POST']) # methods= is allowing GET and POST as methods when submitting the register form
+def register_page():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success') # the 'success' category is from bootstrap and is passed into the layout.html when creating a flash message.
+        return redirect(url_for('home_page')) # will redirect the user to home_page function (/home)
+    return render_template('register2.html', title='Register', form=form)
+
+@app.route('/login')
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+    
---
+++ forms.py
@@ -0,0 +1,32 @@
+# we could put forms in to the html files, but it's best to split things out in to their own files.
+# this way if we need to update it in the future we know exactly where to go.
+# its easier breaking things up in to more manageable chunks of code.
+# these forms are written in python classes then converted to html.
+# each form field will have a list of errors thanks to the wtforms.validators modules. this allows us to open conditionals and print the errors
+
+from flask_wtf import FlaskForm
+from wtforms import StringField #add a string field
+from wtforms import PasswordField # password field
+from wtforms import SubmitField # to submit the form
+from wtforms import BooleanField # for true or false statements, like a Remember Me or Stay Logged In
+from wtforms.validators import DataRequired #add validators input for limiting what a user can put in a field
+from wtforms.validators import Length # add length validation
+from wtforms.validators import Email # email validation. @ .com etc. Must use 'pip install email_validation'
+from wtforms.validators import EqualTo # confirm that 2 inputs are the same (think both password fields)
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)]) # 'Username' is also the label in the html.
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+    
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class SubmitPost(FlaskForm):
+    subject = StringField('Subject', validators=[DataRequired()])
+    body_post = StringField('Body', validators=[DataRequired()])
