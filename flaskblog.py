53a759e65d6362d1b12d9e2cafc3932b44d7b42d
iamrussl <livett.py@gmail.com>
2021-08-05 16:49:58 +0100

    updated login page

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,17 @@
 # flask imports
-from flask import Flask # importing Flask
-from flask import render_template # importing the ability to render web pages
-from flask import url_for #
-from flask import flash # importing to allow for a message to display
-from flask import redirect # importing for the ability to redirect after a GET/POST request on submit, eg after login
+# from flask import Flask # importing Flask
+# from flask import render_template # importing the ability to render web pages
+# from flask import url_for #
+# from flask import flash # importing to allow for a message to display
+# from flask import redirect # importing for the ability to redirect after a GET/POST request on submit, eg after login
+# from forms import RegistrationForm, LoginForm
 
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 # app config
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'removed' # app.config is how you set values on the app
+app.config['SECRET_KEY'] = '6d60bf7b431f47f7dabc0396cf5af41277b8b2bd4b4026001dce7adbdb695a96' # app.config is how you set values on the app
 if __name__ == '__main__':
     app.run(debug=True)
 
@@ -47,10 +49,15 @@ def register_page():
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success') # the 'success' category is from bootstrap and is passed into the layout.html when creating a flash message.
         return redirect(url_for('home_page')) # will redirect the user to home_page function (/home_page)
+
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login_page():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home_page'))
     return render_template('login.html', title='Login', form=form)
     
--- forms.py
+++ forms.py
@@ -4,29 +4,47 @@
 # these forms are written in python classes then converted to html.
 # each form field will have a list of errors thanks to the wtforms.validators modules. this allows us to open conditionals and print the errors
 
+# from flask_wtf import FlaskForm
+# from wtforms import StringField #add a string field
+# from wtforms import PasswordField # password field
+# from wtforms import SubmitField # to submit the form
+# from wtforms import BooleanField # for true or false statements, like a Remember Me or Stay Logged In
+# from wtforms.validators import DataRequired #add validators input for limiting what a user can put in a field
+# from wtforms.validators import Length # add length validation
+# from wtforms.validators import Email # email validation. @ .com etc. Must use 'pip install email_validation'
+# from wtforms.validators import EqualTo # confirm that 2 inputs are the same (think both password fields)
+
 from flask_wtf import FlaskForm
-from wtforms import StringField #add a string field
-from wtforms import PasswordField # password field
-from wtforms import SubmitField # to submit the form
-from wtforms import BooleanField # for true or false statements, like a Remember Me or Stay Logged In
-from wtforms.validators import DataRequired #add validators input for limiting what a user can put in a field
-from wtforms.validators import Length # add length validation
-from wtforms.validators import Email # email validation. @ .com etc. Must use 'pip install email_validation'
-from wtforms.validators import EqualTo # confirm that 2 inputs are the same (think both password fields)
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+# class RegistrationForm(FlaskForm):
+#     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)]) # 'Username' is also the label in the html.
+#     email = StringField('Email', validators=[DataRequired(), Email()])
+#     password = PasswordField('Password', validators=[DataRequired()])
+#     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+#     submit = SubmitField('Sign Up')
+#
+# class LoginForm(FlaskForm):
+#     email = StringField('Email', validators=[DataRequired(), Email()])
+#     password = PasswordField('Password', validators=[DataRequired()])
+#     remember = BooleanField('Remember Me')
+#     submit = SubmitField('Login')
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)]) # 'Username' is also the label in the html.
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-    
+
+
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-
-class SubmitPost(FlaskForm):
-    subject = StringField('Subject', validators=[DataRequired()])
-    body_post = StringField('Body', validators=[DataRequired()])
